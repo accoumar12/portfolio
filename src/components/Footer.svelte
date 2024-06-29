97340d0642c60d5521eb032272abb3c9de3fb62c
Martin Accou <martin.accou@icloud.com>
2024-06-29 21:50:13 +0200

    start to modify the contetn

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,23 +5,11 @@
         Connect with me &darr;
     </p>
     <div class="flex flex-col gap-4 items-center justify-center">
-        <!-- <div
-            class="flex items-center gap-4 sm:gap-6 text-2xl sm:text-3xl md:text-4xl mx-auto"
-        >
-            <a>
-                <i class="fa-brands fa-linkedin-in" />
-            </a>
-            <a>
-                <i class="fa-brands fa-github" />
-            </a>
-        </div>
-        <p class="mx-auto text-sm opacity-70">or</p>
-        <p class="">Hugh_jass@gmail.com</p> -->
-        <p><b class="pr-2">Email</b> samuel_oak@gmail.com</p>
+        <p><b class="pr-2">Email</b>martin.accou@icloud.com</p>
         <p>
             <b class="pr-2">GitHub</b>
             <a href=" " target="_blank" class="text-violet-400"
-                >samuel_oak<sup class=""
+                >accoumar12<sup class=""
                     ><span class="text-xs scale-75 pl-0.5"
                         ><i
                             class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
@@ -33,7 +21,7 @@
         <p>
             <b class="pr-2">LinkedIn</b>
             <a href=" " target="_blank" class="text-violet-400"
-                >samuel_oak<sup class=""
+                >Martin ACCOU<sup class=""
                     ><span class="text-xs scale-75 pl-0.5"
                         ><i
                             class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,9 +4,6 @@
     export let tabs = [
         { name: "Projects ", link: "#projects" },
         { name: "About me", link: "#about" },
-        { name: "Blog", link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" },
-        // { name: "Contact", link: "" },
-        // {name: '', link: ''},
     ];
 </script>
 
@@ -17,7 +14,7 @@
             : " py-6 bg-transparent border-transparent")}
 >
     <h1 class="font-medium">
-        <b class="font-bold poppins">Samuel</b> <span class="">Oak</span>
+        <b class="font-bold poppins">Martin</b> <span class="">ACCOU</span>
     </h1>
     <div class="sm:flex items-center gap-4 hidden">
         {#each tabs as tab, index}
@@ -29,14 +26,6 @@
                 <p>{tab.name}</p>
             </a>
         {/each}
-        <!-- <button
-            class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-violet-600 text-white"
-        >
-            <div
-                class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
-            />
-            <h4 class="relative z-9">Login</h4>
-        </button> -->
         <button
             class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
         >
