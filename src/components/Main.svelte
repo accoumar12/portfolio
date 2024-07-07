94ed97b11bfdfca0c7b341c355db94a1c5ab48eb
Martin Accou <martin.accou@icloud.com>
2024-07-07 20:01:03 +0200

    add my image

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -51,14 +51,12 @@
             class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
         >
             <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
-                Hi! I'm <span class="poppins text-violet-400">Samuel</span> Oak
-                <br />Full Stack
-                <span class="poppins text-violet-400">Developer</span>
+                Hi! I'm <span class="poppins text-violet-400">Martin</span> ACCOU
+                <br />AI/ML
+                <span class="poppins text-violet-400">Engineer</span>
             </h2>
             <p class="text-base sm:text-lg md:text-xl">
-                My <span class="text-violet-400"> favorite tech</span> includes JavaScript
-                (NEXT.JS or SvelteKit), TailwindCSS, Node.js + Express.js & PostgreSQL
-                or Firebase/Firestore!
+            I enjoy implementing <span class="text-violet-400">AI</span> and <span class="text-violet-400">backend</span> solutions, and I also handle some <span class="text-violet-400">frontend</span> work when needed.
             </p>
             <button
                 class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
@@ -69,6 +67,7 @@
                 <h4 class="relative z-9">Get in touch &rarr;</h4>
             </button>
         </div>
+        <!-- TODO: More shiny profile picture-->
         <div class="relative shadow-2xl grid place-items-center">
             <img
                 src={"images/profile.png"}
